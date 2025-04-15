5f861337e65db75416f276240dc0b910789793f6
gs284 Guillaume GONI <guillaume.goni@partnre.com>
2025-04-15 10:17:11 +0200

    texture on triangle level

--- src/rasterizer.rs
+++ src/rasterizer.rs
@@ -4,7 +4,7 @@ use winit::dpi::PhysicalSize;
 
 use crate::{
     maths::{Vec2f, Vec3f, Vec4u},
-    scene::{Camera, Triangle, Vertex, World},
+    scene::{Camera, Texture, Triangle, World},
 };
 
 const SUN_DIRECTION: Vec3f = Vec3f::new(-1., -1., -1.);
@@ -39,22 +39,16 @@ fn world_to_raster(p_world: &Vec3f, cam: &Camera, size: &PhysicalSize<u32>) -> V
     }
 }
 
-fn world_to_raster_vertice(vertice: &Vertex, cam: &Camera, size: &PhysicalSize<u32>) -> Vertex {
-    Vertex {
-        pos: world_to_raster(&vertice.pos, cam, size),
-        color: vertice.color,
-    }
-}
-
 pub fn world_to_raster_triangle(
     triangle: &Triangle,
     cam: &Camera,
     size: &PhysicalSize<u32>,
 ) -> Triangle {
     Triangle {
-        p0: world_to_raster_vertice(&triangle.p0, cam, size),
-        p1: world_to_raster_vertice(&triangle.p1, cam, size),
-        p2: world_to_raster_vertice(&triangle.p2, cam, size),
+        p0: world_to_raster(&triangle.p0, cam, size),
+        p1: world_to_raster(&triangle.p1, cam, size),
+        p2: world_to_raster(&triangle.p2, cam, size),
+        texture: triangle.texture,
     }
 }
 
@@ -67,14 +61,10 @@ pub struct Rect {
 }
 
 fn bounding_box_triangle(t: &Triangle, size: &PhysicalSize<u32>) -> Rect {
-    let min_x =
-        (f64::min(f64::min(t.p0.pos.x, t.p1.pos.x), t.p2.pos.x) as u32).clamp(0, size.width - 1);
-    let max_x =
-        (f64::max(f64::max(t.p0.pos.x, t.p1.pos.x), t.p2.pos.x) as u32).clamp(0, size.width - 1);
-    let min_y =
-        (f64::min(f64::min(t.p0.pos.y, t.p1.pos.y), t.p2.pos.y) as u32).clamp(0, size.height - 1);
-    let max_y =
-        (f64::max(f64::max(t.p0.pos.y, t.p1.pos.y), t.p2.pos.y) as u32).clamp(0, size.height - 1);
+    let min_x = (f64::min(f64::min(t.p0.x, t.p1.x), t.p2.x) as u32).clamp(0, size.width - 1);
+    let max_x = (f64::max(f64::max(t.p0.x, t.p1.x), t.p2.x) as u32).clamp(0, size.width - 1);
+    let min_y = (f64::min(f64::min(t.p0.y, t.p1.y), t.p2.y) as u32).clamp(0, size.height - 1);
+    let max_y = (f64::max(f64::max(t.p0.y, t.p1.y), t.p2.y) as u32).clamp(0, size.height - 1);
 
     Rect {
         x: min_x,
@@ -91,7 +81,7 @@ fn edge_function(ab: Vec3f, tri_a: Vec3f, p: Vec3f) -> f64 {
     ap.x * ab.y - ap.y * ab.x
 }
 
-fn buffer_index(p: Vec3f, size: &PhysicalSize<u32>) -> Option<usize> {
+fn buffer_index(p: &Vec3f, size: &PhysicalSize<u32>) -> Option<usize> {
     if p.x >= 0. && p.x < (size.width as f64) && p.y >= 0. && p.y < (size.height as f64) {
         Some(p.x as usize + p.y as usize * size.width as usize)
     } else {
@@ -102,19 +92,26 @@ fn buffer_index(p: Vec3f, size: &PhysicalSize<u32>) -> Option<usize> {
 fn draw_vertice_basic<B: DerefMut<Target = [u32]>>(
     buffer: &mut B,
     size: &PhysicalSize<u32>,
-    v: &Vertex,
+    v: &Vec3f,
+    texture: &Texture,
 ) {
-    if v.pos.x >= 1.
-        && v.pos.x < (size.width as f64) - 1.
-        && v.pos.y >= 1.
-        && v.pos.y < (size.height as f64) - 1.
-    {
-        if let Some(i) = buffer_index(v.pos, size) {
-            buffer[i] = v.color;
-            buffer[i - 1] = v.color;
-            buffer[i + 1] = v.color;
-            buffer[i - (size.width as usize)] = v.color;
-            buffer[i + (size.width as usize)] = v.color;
+    if v.x >= 1. && v.x < (size.width as f64) - 1. && v.y >= 1. && v.y < (size.height as f64) - 1. {
+        if let Some(i) = buffer_index(v, size) {
+            let color = match texture {
+                Texture::Color(col) => *col,
+                // TODO: Better color calculus
+                Texture::VertexColor(c0, c1, c2) => ((Vec4u::from_color_u32(*c0)
+                    + Vec4u::from_color_u32(*c1)
+                    + Vec4u::from_color_u32(*c2))
+                    / 3.)
+                    .as_color_u32(),
+            };
+
+            buffer[i] = color;
+            buffer[i - 1] = color;
+            buffer[i + 1] = color;
+            buffer[i - (size.width as usize)] = color;
+            buffer[i + (size.width as usize)] = color;
         }
     }
 }
@@ -131,9 +128,9 @@ fn rasterize_triangle<B: DerefMut<Target = [u32]>>(
 
     let bb = bounding_box_triangle(&tri_raster, size);
 
-    let p01 = tri_raster.p1.pos - tri_raster.p0.pos;
-    let p12 = tri_raster.p2.pos - tri_raster.p1.pos;
-    let p20 = tri_raster.p0.pos - tri_raster.p2.pos;
+    let p01 = tri_raster.p1 - tri_raster.p0;
+    let p12 = tri_raster.p2 - tri_raster.p1;
+    let p20 = tri_raster.p0 - tri_raster.p2;
 
     // TODO: not efficient ?
     let tri_area = edge_function(p01, Vec3f::default(), -p20);
@@ -141,8 +138,8 @@ fn rasterize_triangle<B: DerefMut<Target = [u32]>>(
     // Dot product gives negative if two vectors are opposed, so we compare light vector to
     // face normal vector to see if they are opposed (face is lit).
     let sun_norm = SUN_DIRECTION.normalize();
-    let triangle_normal = (triangle.p1.pos - triangle.p0.pos)
-        .cross(&(triangle.p0.pos - triangle.p2.pos))
+    let triangle_normal = (triangle.p1 - triangle.p0)
+        .cross(&(triangle.p0 - triangle.p2))
         .normalize();
     let light = sun_norm
         .dot(&triangle_normal)
@@ -158,9 +155,9 @@ fn rasterize_triangle<B: DerefMut<Target = [u32]>>(
             })
         })
         .for_each(|pixel| {
-            let e01 = edge_function(p01, tri_raster.p0.pos, pixel);
-            let e12 = edge_function(p12, tri_raster.p1.pos, pixel);
-            let e20 = edge_function(p20, tri_raster.p2.pos, pixel);
+            let e01 = edge_function(p01, tri_raster.p0, pixel);
+            let e12 = edge_function(p12, tri_raster.p1, pixel);
+            let e20 = edge_function(p20, tri_raster.p2, pixel);
             if e01 >= 0. && e12 >= 0. && e20 >= 0. {
                 let a12 = e12 / tri_area;
                 let a20 = e20 / tri_area;
@@ -173,11 +170,11 @@ fn rasterize_triangle<B: DerefMut<Target = [u32]>>(
                 // The terms Z1-Z0 and Z2-Z0 can generally be precomputed, which simplifies the computation of Z to two additions and two multiplications. This optimization is worth mentioning because GPUs utilize it, and it's often discussed for essentially this reason.
 
                 // Depth doesn't evolve linearly (its inverse does).
-                let p2_z_inv = 1. / tri_raster.p2.pos.z;
+                let p2_z_inv = 1. / tri_raster.p2.z;
                 let depth = 1.
                     / (p2_z_inv
-                        + (1. / tri_raster.p0.pos.z - p2_z_inv) * a12
-                        + (1. / tri_raster.p1.pos.z - p2_z_inv) * a20);
+                        + (1. / tri_raster.p0.z - p2_z_inv) * a12
+                        + (1. / tri_raster.p1.z - p2_z_inv) * a20);
 
                 // Depth correction of other properties :
                 // Divide each value by the point Z coord and finally multiply by depth.
@@ -186,17 +183,20 @@ fn rasterize_triangle<B: DerefMut<Target = [u32]>>(
                     let index = (pixel.x as usize) + (pixel.y as usize) * size.width as usize;
 
                     if depth < depth_buffer[index] {
-                        let col_2 =
-                            Vec4u::from_color_u32(tri_raster.p2.color) / tri_raster.p2.pos.z;
-                        let col = (col_2
-                            + (Vec4u::from_color_u32(tri_raster.p0.color) / tri_raster.p0.pos.z
-                                - col_2)
-                                * a12
-                            + (Vec4u::from_color_u32(tri_raster.p1.color) / tri_raster.p1.pos.z
-                                - col_2)
-                                * a20)
-                            * depth
-                            * light;
+                        let col = match tri_raster.texture {
+                            Texture::Color(col) => Vec4u::from_color_u32(col),
+                            Texture::VertexColor(c0, c1, c2) => {
+                                // TODO: Better color calculus
+                                let col_2 = Vec4u::from_color_u32(c2) / tri_raster.p2.z;
+
+                                (col_2
+                                    + (Vec4u::from_color_u32(c0) / tri_raster.p0.z - col_2) * a12
+                                    + (Vec4u::from_color_u32(c1) / tri_raster.p1.z - col_2) * a20)
+                                    * depth
+                            }
+                        };
+
+                        let col = col * light;
 
                         buffer[index] = col.as_color_u32();
                         depth_buffer[index] = depth;
@@ -206,9 +206,9 @@ fn rasterize_triangle<B: DerefMut<Target = [u32]>>(
         });
 
     if show_vertices {
-        draw_vertice_basic(buffer, size, &tri_raster.p0);
-        draw_vertice_basic(buffer, size, &tri_raster.p1);
-        draw_vertice_basic(buffer, size, &tri_raster.p2);
+        draw_vertice_basic(buffer, size, &tri_raster.p0, &tri_raster.texture);
+        draw_vertice_basic(buffer, size, &tri_raster.p1, &tri_raster.texture);
+        draw_vertice_basic(buffer, size, &tri_raster.p2, &tri_raster.texture);
     }
 }
 
--- src/scene.rs
+++ src/scene.rs
@@ -1,44 +1,54 @@
+/// # Describing the world...
 use crate::maths::{Rotation, Vec3f};
 
+const DEFAULT_COLOR: u32 = 0xff999999;
+
 #[derive(Debug, Clone, Copy)]
-pub struct Vertex {
-    pub pos: Vec3f,
-    pub color: u32,
+pub enum Texture {
+    /// A simple color for the whole triangle
+    Color(u32),
+    /// A color per vertex in the same order :
+    VertexColor(u32, u32, u32),
+    // Texture, // TODO
 }
 
-impl Vertex {
-    pub fn new<T: Into<f64>>(x: T, y: T, z: T, color: u32) -> Self {
-        Self {
-            pos: Vec3f::new(x.into(), y.into(), z.into()),
-            color,
-        }
+impl Default for Texture {
+    fn default() -> Self {
+        Self::Color(DEFAULT_COLOR)
     }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct Triangle {
-    pub p0: Vertex,
-    pub p1: Vertex,
-    pub p2: Vertex,
+    pub p0: Vec3f,
+    pub p1: Vec3f,
+    pub p2: Vec3f,
+    pub texture: Texture,
 }
 
 impl Default for Triangle {
     fn default() -> Self {
-        Triangle::new(
-            Vertex::new(0., 1., -12., 0xffff0000),
-            Vertex::new(0., 0., -10., 0xff00ff00),
-            Vertex::new(0., 0., -14., 0xff0000ff),
-        )
+        Self {
+            p0: Vec3f::new(0., 1., -12.),
+            p1: Vec3f::new(0., 0., -10.),
+            p2: Vec3f::new(0., 0., -14.),
+            texture: Texture::VertexColor(0xffff0000, 0xff00ff00, 0xff0000ff),
+        }
     }
 }
 
 impl Triangle {
-    fn new(p0: Vertex, p1: Vertex, p2: Vertex) -> Self {
-        Triangle { p0, p1, p2 }
+    pub fn new(p0: Vec3f, p1: Vec3f, p2: Vec3f, texture: Texture) -> Self {
+        Triangle {
+            p0,
+            p1,
+            p2,
+            texture,
+        }
     }
 
     pub fn min_z(&self) -> f64 {
-        f64::min(self.p0.pos.z, f64::min(self.p1.pos.z, self.p2.pos.z))
+        f64::min(self.p0.z, f64::min(self.p1.z, self.p2.z))
     }
 }
 
@@ -72,64 +82,76 @@ impl Default for World {
         let mut triangles = vec![
             Triangle::default(),
             Triangle::new(
-                Vertex::new(3., 0., -19., 0xffff0000),
-                Vertex::new(4., 0., -19., 0xffff0000),
-                Vertex::new(4., 1., -10., 0xffff0000),
+                Vec3f::new(3., 0., -19.),
+                Vec3f::new(4., 0., -19.),
+                Vec3f::new(4., 1., -10.),
+                Texture::Color(0xffff0000),
             ),
             Triangle::new(
-                Vertex::new(4., 0., -19., 0xffff0000),
-                Vertex::new(5., 0., -19., 0xffff0000),
-                Vertex::new(4., 1., -10., 0xffff0000),
+                Vec3f::new(4., 0., -19.),
+                Vec3f::new(5., 0., -19.),
+                Vec3f::new(4., 1., -10.),
+                Texture::Color(0xffff0000),
             ),
             Triangle::new(
-                Vertex::new(3., 2., -19., 0xff0000ff),
-                Vertex::new(4., 1., -10., 0xff0000ff),
-                Vertex::new(4., 2., -19., 0xff0000ff),
+                Vec3f::new(3., 2., -19.),
+                Vec3f::new(4., 1., -10.),
+                Vec3f::new(4., 2., -19.),
+                Texture::Color(0xff0000ff),
             ),
             Triangle::new(
-                Vertex::new(4., 1., -10., 0xff0000ff),
-                Vertex::new(5., 2., -19., 0xff0000ff),
-                Vertex::new(4., 2., -19., 0xff0000ff),
+                Vec3f::new(4., 1., -10.),
+                Vec3f::new(5., 2., -19.),
+                Vec3f::new(4., 2., -19.),
+                Texture::Color(0xff0000ff),
             ),
             Triangle::new(
-                Vertex::new(3., 0., -19., 0xff00ff00),
-                Vertex::new(4., 1., -10., 0xff00ff00),
-                Vertex::new(3., 1., -19., 0xff00ff00),
+                Vec3f::new(3., 0., -19.),
+                Vec3f::new(4., 1., -10.),
+                Vec3f::new(3., 1., -19.),
+                Texture::Color(0xff00ff00),
             ),
             Triangle::new(
-                Vertex::new(3., 2., -19., 0xff00ff00),
-                Vertex::new(3., 1., -19., 0xff00ff00),
-                Vertex::new(4., 1., -10., 0xff00ff00),
+                Vec3f::new(3., 2., -19.),
+                Vec3f::new(3., 1., -19.),
+                Vec3f::new(4., 1., -10.),
+                Texture::Color(0xff00ff00),
             ),
             Triangle::new(
-                Vertex::new(5., 1., -19., 0xffffff00),
-                Vertex::new(4., 1., -10., 0xffffff00),
-                Vertex::new(5., 0., -19., 0xffffff00),
+                Vec3f::new(5., 1., -19.),
+                Vec3f::new(4., 1., -10.),
+                Vec3f::new(5., 0., -19.),
+                Texture::Color(0xffffff00),
             ),
             Triangle::new(
-                Vertex::new(4., 1., -10., 0xffffff00),
-                Vertex::new(5., 1., -19., 0xffffff00),
-                Vertex::new(5., 2., -19., 0xffffff00),
+                Vec3f::new(4., 1., -10.),
+                Vec3f::new(5., 1., -19.),
+                Vec3f::new(5., 2., -19.),
+                Texture::Color(0xffffff00),
             ),
             Triangle::new(
-                Vertex::new(2., 0.5, -19., 0xff00ffff),
-                Vertex::new(4., 0.5, -15., 0xff00ffff),
-                Vertex::new(2., 1.5, -19., 0xff00ffff),
+                Vec3f::new(2., 0.5, -19.),
+                Vec3f::new(4., 0.5, -15.),
+                Vec3f::new(2., 1.5, -19.),
+                Texture::Color(0xff00ffff),
             ),
             Triangle::new(
-                Vertex::new(4., 0.5, -15., 0xff00ffff),
-                Vertex::new(4., 1.5, -15., 0xff00ffff),
-                Vertex::new(2., 1.5, -19., 0xff00ffff),
+                Vec3f::new(4., 0.5, -15.),
+                Vec3f::new(4., 1.5, -15.),
+                Vec3f::new(2., 1.5, -19.),
+                Texture::Color(0xff00ffff),
             ),
             Triangle::new(
-                Vertex::new(3.7, 0.7, -12., 0xffff00ff),
-                Vertex::new(4.3, 0.7, -12., 0xffff00ff),
-                Vertex::new(3.7, 1.3, -12., 0xffff00ff),
+                Vec3f::new(3.7, 0.7, -12.),
+                Vec3f::new(4.3, 0.7, -12.),
+                Vec3f::new(3.7, 1.3, -12.),
+                Texture::Color(0xffff00ff),
             ),
             Triangle::new(
-                Vertex::new(4.3, 0.7, -12., 0xffff00ff),
-                Vertex::new(4.3, 1.3, -12., 0xffff00ff),
-                Vertex::new(3.7, 1.3, -12., 0xffff00ff),
+                Vec3f::new(4.3, 0.7, -12.),
+                Vec3f::new(4.3, 1.3, -12.),
+                Vec3f::new(3.7, 1.3, -12.),
+                Texture::Color(0xffff00ff),
             ),
         ];
 
@@ -143,7 +165,6 @@ impl Default for World {
 }
 
 mod obj {
-    const DEFAULT_COLOR: u32 = 0xff999999;
     pub const SUZANNE_OBJ_PATH: &str = "resources/Suzanne.obj";
 
     use std::{collections::HashMap, fs::File, io::BufReader, path::Path};
@@ -154,9 +175,9 @@ mod obj {
         parse_mtl, parse_obj,
     };
 
-    use crate::scene::Vertex;
+    use crate::maths::Vec3f;
 
-    use super::Triangle;
+    use super::{Texture, Triangle};
 
     // TODO: better error handling
     pub fn import_triangles_and_diffuse<P: AsRef<Path>>(
@@ -180,15 +201,16 @@ mod obj {
 
         // TODO: différents groupes, materiaux on vetrices, ...
         for (poly_index, poly) in obj.polygons.iter().enumerate() {
-            let color = find_mtl_color(&obj.meshes, &mtls, poly_index).unwrap_or(DEFAULT_COLOR);
-            triangles.push(polygon_to_triangle(&obj.positions[..], color, poly));
+            let texture =
+                find_mtl_texture(&obj.meshes, &mtls, poly_index).unwrap_or(Default::default());
+            triangles.push(polygon_to_triangle(&obj.positions[..], texture, poly));
         }
     }
 
     fn load_materials_diffuse_rgb<P: AsRef<Path>>(
         obj_path: P,
         mtl_librairies: &[String],
-    ) -> HashMap<String, u32> {
+    ) -> HashMap<String, Texture> {
         let mut mtls = HashMap::new();
         for mtl_name in mtl_librairies.iter() {
             let path = obj_path
@@ -212,10 +234,12 @@ mod obj {
                 if let Some(MtlColor::Rgb(r, g, b)) = m.diffuse {
                     mtls.insert(
                         mtl_name,
-                        0xff000000
-                            | (((r * 255.) as u32) << 16)
-                            | (((g * 255.) as u32) << 8)
-                            | ((b * 255.) as u32),
+                        Texture::Color(
+                            0xff000000
+                                | (((r * 255.) as u32) << 16)
+                                | (((g * 255.) as u32) << 8)
+                                | ((b * 255.) as u32),
+                        ),
                     );
                 } else {
                     unimplemented!(
@@ -230,11 +254,11 @@ mod obj {
         mtls
     }
 
-    fn find_mtl_color(
+    fn find_mtl_texture(
         meshes: &HashMap<String, Group>,
-        materials: &HashMap<String, u32>,
+        materials: &HashMap<String, Texture>,
         polygon_index: usize,
-    ) -> Option<u32> {
+    ) -> Option<Texture> {
         meshes
             .iter()
             .find_map(|(mtl_name, grp)| {
@@ -248,23 +272,23 @@ mod obj {
 
     fn polygon_to_triangle(
         positions: &[(f32, f32, f32, f32)],
-        color: u32,
+        texture: Texture,
         poly: &Polygon,
     ) -> Triangle {
-        let map = |pos_index: usize| -> Vertex {
+        let map = |pos_index: usize| -> Vec3f {
             let (x, y, z, _) = positions[pos_index];
-            Vertex::new(x, y, z, color)
+            Vec3f::new(x as f64, y as f64, z as f64)
         };
 
         match poly {
             Polygon::P(vec) if vec.len() == 3 => {
-                Triangle::new(map(vec[0]), map(vec[1]), map(vec[2]))
+                Triangle::new(map(vec[0]), map(vec[1]), map(vec[2]), texture)
             }
             Polygon::PT(vec) | Polygon::PN(vec) if vec.len() == 3 => {
-                Triangle::new(map(vec[0].0), map(vec[1].0), map(vec[2].0))
+                Triangle::new(map(vec[0].0), map(vec[1].0), map(vec[2].0), texture)
             }
             Polygon::PTN(vec) if vec.len() == 3 => {
-                Triangle::new(map(vec[0].0), map(vec[1].0), map(vec[2].0))
+                Triangle::new(map(vec[0].0), map(vec[1].0), map(vec[2].0), texture)
             }
             _ => panic!("Model should be triangulated first to be loaded properly"),
         }
