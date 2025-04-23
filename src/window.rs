0278e57e99f5601a5c3df4f8e3ccf8f90b6b9d56
gs284 Guillaume GONI <guillaume.goni@partnre.com>
2025-04-23 16:29:19 +0200

    blah

--- src/window.rs
+++ src/window.rs
@@ -1,7 +1,10 @@
 use std::{
     num::NonZeroU32,
     rc::Rc,
-    sync::{Arc, atomic::AtomicU32},
+    sync::{
+        Arc,
+        atomic::{AtomicBool, AtomicU32},
+    },
     time::Instant,
 };
 
@@ -36,7 +39,7 @@ pub struct App {
     world: World,
     cursor: Option<PhysicalPosition<f64>>,
     mouse_left_held: bool,
-    depth_buffer: Arc<[f32]>,
+    buffers_depth_lock: Option<(Arc<[AtomicU32]>, Arc<[AtomicBool]>)>,
     settings: Settings,
 }
 
@@ -227,6 +230,8 @@ impl ApplicationHandler for App {
                 buffer.fill(0xff181818);
                 let buffer_fill = Instant::now().duration_since(t).as_millis();
 
+                let (depth_buffer, lock_buffer) =
+                    self.buffers_depth_lock.take().unwrap_or_else(|| {});
                 let tot_size = (size.width * size.height) as usize;
                 let mut local_depth_buffer = Vec::with_capacity(tot_size);
                 local_depth_buffer.resize_with(tot_size, || {
@@ -235,9 +240,7 @@ impl ApplicationHandler for App {
                 let local_depth_buffer = Arc::new(local_depth_buffer.into_boxed_slice());
 
                 let mut local_lock_buffer = Vec::with_capacity(tot_size);
-                local_lock_buffer.resize_with(tot_size, || {
-                    AtomicU32::new(u32::from_le_bytes(f32::INFINITY.to_le_bytes()))
-                });
+                local_lock_buffer.resize_with(tot_size, || AtomicBool::new(false));
                 let local_lock_buffer = Arc::new(local_lock_buffer.into_boxed_slice());
 
                 let t = Instant::now();
