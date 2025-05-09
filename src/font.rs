25f837bca006dea8ed6df687b699a611600ec9e5
gs284 Guillaume GONI <guillaume.goni@partnre.com>
2025-05-09 17:11:52 +0200

    merge fonts

--- src/font.rs
+++ src/font.rs
@@ -1,6 +1,7 @@
-use std::ops::DerefMut;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 use fontdue::{Font, FontSettings};
+use rayon::prelude::*;
 use winit::dpi::PhysicalSize;
 
 const PX: f32 = 12.;
@@ -21,41 +22,52 @@ impl Default for TextWriter {
 }
 
 impl TextWriter {
-    pub fn rasterize<B: DerefMut<Target = [u32]>>(
-        &mut self,
-        buffer: &mut B,
+    fn rasterize_generic(
+        &self,
         size: PhysicalSize<u32>,
-        text: &str,
+        i: usize,
+        l: &str,
+        mut set_buffer: impl FnMut(usize, u32),
     ) {
-        let mut start_y = BASE_Y;
-        text.lines().for_each(|l| {
-            let mut start_x = BASE_X;
-            l.chars().for_each(|c| {
-                let (metrics, image) = self.font.rasterize(c, PX);
+        let mut start_x = BASE_X;
+        let start_y = BASE_Y + i * PX as usize;
 
-                if metrics.width > 0 {
-                    image
-                        .chunks(metrics.width)
-                        .enumerate()
-                        .flat_map(|(y, l)| l.iter().enumerate().map(move |(x, p)| (x, y, p)))
-                        .for_each(|(x, y, p)| {
-                            let i = metrics.xmin as isize + x as isize + start_x as isize;
-                            let j = PX as isize - metrics.height as isize - metrics.ymin as isize
-                                + y as isize
-                                + start_y as isize;
-                            if i >= 0
-                                && i < size.width as isize
-                                && j >= 0
-                                && j < size.height as isize
-                            {
-                                buffer[i as usize + j as usize * size.width as usize] |=
-                                    0xff000000 | ((0x00ffffff * (*p as u32)) / 255);
-                            }
-                        });
-                }
-                start_x += metrics.advance_width.ceil() as usize;
+        l.chars().for_each(|c| {
+            let (metrics, image) = self.font.rasterize(c, PX);
+
+            if metrics.width > 0 {
+                image
+                    .chunks(metrics.width)
+                    .enumerate()
+                    .flat_map(|(y, l)| l.iter().enumerate().map(move |(x, p)| (x, y, p)))
+                    .for_each(|(x, y, p)| {
+                        let i = metrics.xmin as isize + x as isize + start_x as isize;
+                        let j = PX as isize - metrics.height as isize - metrics.ymin as isize
+                            + y as isize
+                            + start_y as isize;
+                        if i >= 0 && i < size.width as isize && j >= 0 && j < size.height as isize {
+                            set_buffer(
+                                i as usize + j as usize * size.width as usize,
+                                0xff000000 | ((0x00ffffff * (*p as u32)) / 255),
+                            );
+                        }
+                    });
+            }
+            start_x += metrics.advance_width.ceil() as usize;
+        });
+    }
+
+    pub fn rasterize(&self, buffer: &mut [u32], size: PhysicalSize<u32>, text: &str) {
+        text.lines().enumerate().for_each(|(i, l)| {
+            self.rasterize_generic(size, i, l, |index, color| buffer[index] |= color);
+        });
+    }
+
+    pub fn rasterize_par(&self, buffer: &[AtomicU64], size: PhysicalSize<u32>, text: &str) {
+        text.lines().enumerate().par_bridge().for_each(|(i, l)| {
+            self.rasterize_generic(size, i, l, |index, color| {
+                buffer[index].fetch_and(color as u64, Ordering::Relaxed);
             });
-            start_y += PX as usize;
         });
     }
 }
