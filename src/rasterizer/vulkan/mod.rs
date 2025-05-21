cbe361af2b836fa3dce0a1c1736c0ff9cec5079a
gs284 Guillaume GONI <guillaume.goni@partnre.com>
2025-05-21 16:53:54 +0200

    start init vulkan

--- src/rasterizer/vulkan/mod.rs
+++ src/rasterizer/vulkan/mod.rs
@@ -1,6 +1,11 @@
-use std::ops::DerefMut;
+use std::{
+    ffi::{CStr, CString},
+    ops::DerefMut,
+    rc::Rc,
+};
 
-use winit::dpi::PhysicalSize;
+use ash::{Entry, Instance, LoadingError, vk};
+use winit::{dpi::PhysicalSize, window::Window};
 
 use crate::{font::TextWriter, scene::World, window::AppObserver};
 
@@ -9,8 +14,21 @@ use super::settings::Settings;
 #[cfg(feature = "stats")]
 use super::Stats;
 
-#[derive(Default, Debug, Clone, Copy)]
-pub struct VulkanEngine {}
+const APP_NAME: &CStr = c"My rasterizer";
+
+pub struct VulkanEngine {
+    window: Window,
+
+    // TODO: convert to Rust way...
+    frame_number: usize,
+    window_extent: vk::Extent2D,
+
+    instance: Instance,
+    debug_messenger: vk::DebugUtilsMessengerEXT,
+    chosen_gpu: vk::PhysicalDevice,
+    device: vk::Device,
+    surface: vk::SurfaceKHR,
+}
 
 impl VulkanEngine {
     pub fn rasterize<B: DerefMut<Target = [u32]>>(
@@ -24,5 +42,65 @@ impl VulkanEngine {
         #[cfg(feature = "stats")] _stats: &mut Stats,
     ) {
         todo!();
+    }
+
+    fn init(window: Window) -> Self {
+        let instance = Self::init_vulkan();
+
+        // let mut res = Self {
+        //     window,
+
+        //     frame_number: 0,
+        //     // window_extent: vk::Extent2D,
+
+        //     // instance: vk::Instance,
+        //     // debug_messenger: vk::DebugUtilsMessengerEXT,
+        //     // chosen_gpu: vk::PhysicalDevice,
+        //     // device: vk::Device,
+        //     // surface: vk::SurfaceKHR,
+        // };
+        todo!();
+
+        // Self::init_vulkan();
+        // Self::init_swapchain();
+        // Self::init_commands();
+        // Self::init_sync_structures();
+    }
+
+    fn init_vulkan() -> vk::Instance {
+        let entry = unsafe { Entry::load().unwrap() };
+        let app_info = vk::ApplicationInfo::default()
+            .api_version(vk::make_api_version(1, 3, 0, 0))
+            .application_name(APP_NAME);
+        // TODO: other parameters : engine name + version, ...
+        let create_info = vk::InstanceCreateInfo {
+            p_application_info: &app_info,
+            ..Default::default()
+        };
+        let instance = unsafe { entry.create_instance(&create_info, None).unwrap() };
+
+        // TODO: look into ash-examples/src/lib
+        // TODO: validation layers + default debug messenger
+
+        todo!();
+    }
+
+    fn init_swapchain(&mut self) {
+        todo!();
+    }
+
+    fn init_commands(&mut self) {
+        todo!();
+    }
+
+    fn init_sync_structures(&mut self) {
+        todo!();
+    }
+}
+
+impl Drop for VulkanEngine {
+    fn drop(&mut self) {
+        unsafe { self.instance.destroy_instance(None) };
+        todo!()
     }
 }
