0e37026164e92bddba37e1e9c2c6702f2eb4de12
GONI Guillaume <g.goni@laposte.net>
2025-07-07 14:54:52 +0200

    test android termux

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+use winit::{
+    application::ApplicationHandler,
+    event::{ElementState, KeyEvent, WindowEvent},
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowId},
+    platform::android::{activity::AndroidApp,
+    EventLoopBuilderExtAndroid}
+};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        state: ElementState::Pressed,
+                        repeat: false,
+                        ..
+                    },
+                ..
+            } => {
+                event_loop.exit();
+            }
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
+
+pub fn main() {
+    let mut event_loop = EventLoop::builder();
+    
+    let event_loop = event_loop.build().unwrap();
+    // ControlFlow::Poll : Run in a loop (game)
+    // Wait : Runs only on event (apps)
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
+
+#[unsafe(no_mangle)]
+fn android_main(app: AndroidApp) {
+ let mut event_loop = EventLoop::builder();
+    event_loop.with_android_app(app);
+    
+    let event_loop = event_loop.build().unwrap();
+    // ControlFlow::Poll : Run in a loop (game)
+    // Wait : Runs only on event (apps)
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
